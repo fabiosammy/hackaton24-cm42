9d898adff159211264ff90321f72d6c5d11540f1
fabiosammy <fabiosammy@gmail.com>
2024-11-23 09:02:55 -0300

    Load the migration and the sqlalchemy

--- hackaton/__init__.py
+++ hackaton/__init__.py
@@ -1,13 +1,18 @@
 import os
 
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+db = SQLAlchemy()
+migrate = Migrate()
 
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'hackaton.sqlite'),
+        SQLALCHEMY_DATABASE_URI=os.getenv('DATABASE_URL')
     )
 
     if test_config is None:
@@ -22,6 +27,10 @@ def create_app(test_config=None):
         os.makedirs(app.instance_path)
     except OSError:
         pass
+
+    # Registering extensions
+    db.init_app(app)
+    migrate.init_app(app, db)
 
     # a simple page that says hello
     @app.route('/hello')
