29bddb59193bef605b548e975d73080db15d4cf5
fabiosammy <fabiosammy@gmail.com>
2024-11-23 15:06:40 -0300

    Load a SECRET_KEY from os env instead of generating a new one

--- hackaton/__init__.py
+++ hackaton/__init__.py
@@ -10,7 +10,7 @@ def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
-        SECRET_KEY='dev',
+        SECRET_KEY=os.getenv('SECRET_KEY'),
         SQLALCHEMY_DATABASE_URI=os.getenv('DATABASE_URL')
     )
 
